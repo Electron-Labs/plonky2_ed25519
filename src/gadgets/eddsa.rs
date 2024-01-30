d3d79c669a8efaf26054ddc02166bcc51cd611ed
electron-team <utsavjn48@gmail.com>
2024-01-30 12:36:23 +0530

    feat: sig verify directly using sha blocks
    Signed-off-by: electron-team <utsavjn48@gmail.com>

--- src/gadgets/eddsa.rs
+++ src/gadgets/eddsa.rs
@@ -5,7 +5,9 @@ use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::field::extension::Extendable;
 use plonky2_crypto::biguint::BigUintTarget;
 use plonky2_crypto::u32::arithmetic_u32::U32Target;
+use plonky2_sha512::gadgets::sha512;
 use plonky2_sha512::gadgets::sha512::{array_to_bits, make_sha512_circuit};
+use crate::curve::curve_types::Curve;
 use crate::curve::ed25519::Ed25519;
 use crate::field::ed25519_scalar::Ed25519Scalar;
 use crate::gadgets::curve::CircuitBuilderCurve;
@@ -70,6 +72,31 @@ fn connect_bool_targets<F: RichField + Extendable<D>, const D: usize>(
     }
 }
 
+pub fn verify_using_preprocessed_sha_block<F: RichField + Extendable<D>, const D: usize>(
+    builder: &mut CircuitBuilder<F, D>,
+    sha_preprocessed: &Vec<BoolTarget>,
+    pub_key: &Vec<BoolTarget>,
+    sig: &Vec<BoolTarget>,
+) {
+    let digest = sha512::sha512_circuit_with_preprocessed_input(builder, sha_preprocessed);
+    let digest_bits_le = bits_in_le(digest.clone());
+    let digest_biguint = bits_to_biguint_target(builder, digest_bits_le);
+    let h_scalar = builder.reduce::<Ed25519Scalar>(&digest_biguint);
+    let s_sig_bits_le = bits_in_le(sig[256..512].to_vec());
+    let pk_bits_le = bits_in_le(pub_key.clone());
+    let point_a = builder.point_decompress::<Ed25519>(&pk_bits_le);
+    let point_ha = builder.curve_scalar_mul_windowed(&point_a, &h_scalar);
+    let r_bits_le = bits_in_le(sig[..256].to_vec());
+    let point_r = builder.point_decompress(&r_bits_le);
+    let generator_target = builder.constant_affine_point(Ed25519::GENERATOR_AFFINE);
+    let s_sig_biguint = bits_to_biguint_target(builder, s_sig_bits_le);
+    let s_sig_nonNative = builder.biguint_to_nonnative(&s_sig_biguint);
+    let point_sb = builder.curve_scalar_mul_windowed(&generator_target, &s_sig_nonNative);
+    let rhs = builder.curve_add(&point_r, &point_ha);
+
+    builder.connect_affine_point(&point_sb, &rhs);
+}
+
 pub fn make_verify_circuits<F: RichField + Extendable<D>, const D: usize>(
     builder: &mut CircuitBuilder<F, D>,
     msg_len: usize,
@@ -107,7 +134,10 @@ pub fn make_verify_circuits<F: RichField + Extendable<D>, const D: usize>(
     let point_ha = builder.curve_scalar_mul_windowed(&point_a, &h_scalar);
     let r_bits_le = bits_in_le(sig[..256].to_vec());
     let point_r = builder.point_decompress(&r_bits_le);
-    let point_sb = builder.fixed_base_curve_scalar_mul_comb(&s_sig_bits_le);
+    let generator_target = builder.constant_affine_point(Ed25519::GENERATOR_AFFINE);
+    let s_sig_biguint = bits_to_biguint_target(builder, s_sig_bits_le);
+    let s_sig_nonNative = builder.biguint_to_nonnative(&s_sig_biguint);
+    let point_sb = builder.curve_scalar_mul_windowed(&generator_target, &s_sig_nonNative);
     let rhs = builder.curve_add(&point_r, &point_ha);
 
     builder.connect_affine_point(&point_sb, &rhs);
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use anyhow::Result;
 use std::time::Instant;
 use plonky2::hash::hash_types::RichField;
-use plonky2::iop::witness::PartialWitness;
+use plonky2::iop::witness::{PartialWitness, Witness, WitnessWrite};
 use plonky2::plonk::circuit_builder::CircuitBuilder;
 use plonky2::plonk::circuit_data::{
     CircuitConfig, CommonCircuitData, VerifierOnlyCircuitData,
@@ -14,14 +14,64 @@ use plonky2::plonk::proof::ProofWithPublicInputs;
 use plonky2_ed25519::curve::eddsa::{
     SAMPLE_MSG1, SAMPLE_PK1, SAMPLE_SIG1, 
 };
-use plonky2_ed25519::gadgets::eddsa::{fill_circuits, make_verify_circuits};
+use plonky2_ed25519::gadgets::eddsa::{fill_circuits, make_verify_circuits, verify_using_preprocessed_sha_block};
 use plonky2::field::extension::Extendable;
+use plonky2::iop::target::BoolTarget;
+use plonky2_sha512::gadgets::sha512::array_to_bits;
 
 type ProofTuple<F, C, const D: usize> = (
     ProofWithPublicInputs<F, C, D>,
     VerifierOnlyCircuitData<C, D>,
     CommonCircuitData<F, D>,
 );
+
+fn prove_ed25519_from_sha512_block<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>(
+    sha_preprocessed: &Vec<u8>,
+    pub_key: &Vec<u8>,
+    sig: &Vec<u8>
+)
+    where
+        [(); C::Hasher::HASH_SIZE]:,
+{
+    let mut builder = CircuitBuilder::<F, D>::new(CircuitConfig::wide_ecc_config());
+    let sha_block_bits = array_to_bits(sha_preprocessed);
+    let pub_key_bits = array_to_bits(pub_key);
+    let sig_bits = array_to_bits(sig);
+
+    let mut sha_block_target = Vec::<BoolTarget>::new();
+    let mut pub_key_target = Vec::<BoolTarget>::new();
+    let mut sig_target = Vec::<BoolTarget>::new();
+
+    for i in 0..2048 {
+        sha_block_target.push(builder.add_virtual_bool_target_unsafe());
+    }
+    for i in 0..256 {
+        pub_key_target.push(builder.add_virtual_bool_target_unsafe());
+    }
+    for i in 0..512 {
+        sig_target.push(builder.add_virtual_bool_target_unsafe());
+    }
+    verify_using_preprocessed_sha_block(&mut builder, &sha_block_target, &pub_key_target, &sig_target);
+    println!("Starting to build the circuit with num gates {:?}", builder.num_gates());
+    let data = builder.build::<C>();
+    println!("Circuit built");
+
+    let mut pw = PartialWitness::new();
+    for i in 0..2048 {
+        pw.set_bool_target(sha_block_target[i], sha_block_bits[i]);
+    }
+    for i in 0..512 {
+        pw.set_bool_target(sig_target[i], sig_bits[i]);
+    }
+    for i in 0..256 {
+        pw.set_bool_target(pub_key_target[i], pub_key_bits[i]);
+    }
+    println!("Starting proof gen..");
+    let s1 = Instant::now();
+    let proof = data.prove(pw).unwrap();
+    println!("Time taken to generate the proof {:?}", s1.elapsed());
+    data.verify(proof.clone()).expect("verify error");
+}
 
 fn prove_ed25519<F: RichField + Extendable<D>, C: GenericConfig<D, F = F>, const D: usize>(
     msg: &[u8],
@@ -68,3 +118,14 @@ fn main() -> Result<()> {
     let _ = benchmark();
     Ok(())
 }
+
+// [110, 8, 2, 17, 253, 252, 197,0,0,"9":0,"10":0,"11":0,"12":34,"13":72,"14":10,"15":32,"16":122,"17":142,"18":192,"19":235,"20":60,"21":200,"22":129,"23":138,"24":195,"25":28,"26":210,"27":246,"28":239,"29":120,"30":205,"31":133,"32":142,"33":55,"34":139,"35":49,"36":122,"37":88,"38":39,"39":159,"40":168,"41":141,"42":149,"43":188,"44":97,"45":173,"46":187,"47":96,"48":18,"49":36,"50":8,"51":2,"52":18,"53":32,"54":112,"55":168,"56":36,"57":7,"58":51,"59":201,"60":176,"61":92,"62":83,"63":27,"64":128,"65":6,"66":184,"67":203,"68":242,"69":148,"70":52,"71":222,"72":164,"73":187,"74":23,"75":226,"76":230,"77":212,"78":78,"79":193,"80":83,"81":74,"82":85,"83":83,"84":213,"85":154,"86":42,12,"88":8,"89":151,"90":206,"91":180,"92":172,"93":6,"94":16,"95":190,"96":144,"97":201,"98":199,"99":1,"100":50,"101":9,"102":111,"103":115,"104":109,"105":111,"106":115,"107":105,"108":115,"109":45,"110":49}
+// msg [109, 8, 2, 17, 253, 252, 197, 0, 0, 0, 0, 0, 34, 72, 10, 32, 122, 142, 192, 235, 60, 200, 129, 138, 195, 28, 210, 246, 239, 120, 205, 133, 142, 55, 139, 49, 122, 88, 39, 159, 168, 141, 149, 188, 97, 173, 187, 96, 18, 36, 8, 2, 18, 32, 112, 168, 36, 7, 51, 201, 176, 92, 83, 27, 128, 6, 184, 203, 242, 148, 52, 222, 164, 187, 23, 226, 230, 212, 78, 193, 83, 74, 85, 83, 213, 154,                                                                                                                                                                                                                                                                                                                  42, 11, 8, 151, 206, 180, 172, 6, 16,                               211, 193, 250, 19, 50, 9, 111, 115, 109, 111, 115, 105, 115, 45, 49]
+// sig [226, 162, 75, 243, 144, 204, 243, 68, 174, 40, 63, 58, 251, 123, 153, 105, 120, 238, 35, 63, 216, 100, 180, 171, 119, 149, 138, 252, 156, 7, 199, 108, 95, 59, 25, 183, 119, 161, 51, 46, 46, 147, 9, 31, 113, 191, 173, 81, 126, 238, 51, 91, 243, 227, 118, 46, 132, 191, 32, 218, 233, 51, 34, 6]
+// pub key [232, 220, 244, 245, 129, 135, 207, 5, 177, 141, 204, 198, 208, 136, 74, 224, 139, 244, 169, 141, 136, 113, 125, 15, 255, 146, 162, 182, 244, 87, 77, 71]
+// signed_msg [226, 162, 75, 243, 144, 204, 243, 68, 174, 40, 63, 58, 251, 123, 153, 105, 120, 238, 35, 63, 216, 100, 180, 171, 119, 149, 138, 252, 156, 7, 199, 108, 232, 220, 244, 245, 129, 135, 207, 5, 177, 141, 204, 198, 208, 136, 74, 224, 139, 244, 169, 141, 136, 113, 125, 15, 255, 146, 162, 182, 244, 87, 77, 71, 109, 8, 2, 17, 253, 252, 197, 0, 0, 0, 0, 0, 34, 72, 10, 32, 122, 142, 192, 235, 60, 200, 129, 138, 195, 28, 210, 246, 239, 120, 205, 133, 142, 55, 139, 49, 122, 88, 39, 159, 168, 141, 149, 188, 97, 173, 187, 96, 18, 36, 8, 2, 18, 32, 112, 168, 36, 7, 51, 201, 176, 92, 83, 27, 128, 6, 184, 203, 242, 148, 52, 222, 164, 187, 23, 226, 230, 212, 78, 193, 83, 74, 85, 83, 213, 154, 42, 11, 8, 151, 206, 180, 172, 6, 16, 211, 193, 250, 19, 50, 9, 111, 115, 109, 111, 115, 105, 115, 45, 49]
+
+
+// [109, 8, 2, 17, 253, 252, 197, 0, 0, 0, 0, 0, 34, 72, 10, 32, 122, 142, 192, 235, 60, 200, 129, 138, 195, 28, 210, 246, 239, 120, 205, 133, 142, 55, 139, 49, 122, 88, 39, 159, 168, 141, 149, 188, 97, 173, 187, 96, 18, 36, 8, 2, 18, 32, 112, 168, 36, 7, 51, 201, 176, 92, 83, 27, 128, 6, 184, 203, 242, 148, 52, 222, 164, 187, 23, 226, 230, 212, 78, 193, 83, 74, 85, 83, 213, 154, 42, 11, 8, 151, 206, 180, 172, 6, 16, 211, 193, 250, 19, 50, 9, 111, 115, 109, 111, 115, 105, 115, 45, 49]
+// [110, 8, 2, 17, 253, 252, 197, 0, 0, 0, 0, 0, 34, 72, 10, 32, 122, 142, 192, 235, 60, 200, 129, 138, 195, 28, 210, 246, 239, 120, 205, 133, 142, 55, 139, 49, 122, 88, 39, 159, 168, 141, 149, 188, 97, 173, 187, 96, 18, 36, 8, 2, 18, 32, 112, 168, 36, 7, 51, 201, 176, 92, 83, 27, 128, 6, 184, 203, 242, 148, 52, 222, 164, 187, 23, 226, 230, 212, 78, 193, 83, 74, 85, 83, 213, 154, 42, 12, 8, 151, 206, 180, 172, 6, 16, 190, 144, 201, 199, 1, 50, 9, 111, 115, 109, 111, 115, 105, 115, 45, 49]
+// [109 8 2 17 253 252 197 0 0 0 0 0 34 72 10 32 122 142 192 235 60 200 129 138 195 28 210 246 239 120 205 133 142 55 139 49 122 88 39 159 168 141 149 188 97 173 187 96 18 36 8 2 18 32 112 168 36 7 51 201 176 92 83 27 128 6 184 203 242 148 52 222 164 187 23 226 230 212 78 193 83 74 85 83 213 154 42 11 8 151 206 180 172 6 16 211 193 250 19 50 9 111 115 109 111 115 105 115 45 49]
